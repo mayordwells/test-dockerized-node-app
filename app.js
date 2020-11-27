5533b6d9b1229bbad2b328d08dfa09c46c1c5d74
Mayowa Adewole <adewolemayowa@gmail.com>
2020-11-27 11:47:13 +0300

    update server code and task definition

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const express = require('express');
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+// const HOST = '0.0.0.0';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World from Node.js app!');
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+// app.listen(PORT, HOST);
+// console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log('Server is running on port 3000');
+});
